26423b80c36554a19d0205178023bc1aa393e1a1
Hugolifes <rach.rc36@gmail.com>
2022-10-11 16:15:36 -0600

    changes

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^', include('ProjectApp.urls'))
+    re_path(r'^', include('ProjectApp.urls'))
 ]
